1f4ea2d0888adcd3f5cbee442563caee6573cefc
ayakosomethingxxx@yahoo.co.jp <ayakosomethingxxx@gmail.com>
2020-12-28 03:06:33 +0900

    fix

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 const BlogPostTemplate = ({ data, location }) => {
   const post = data.markdownRemark
