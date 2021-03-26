b5a367a745e15701f761c3a5f60773a238b8be37
ayakotanaka0224 <ayakosomethingxxx@gmail.com>
2021-03-26 17:18:02 +0900

    ogp変更

--- src/components/seo.js
+++ src/components/seo.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
-import ogpThumbnail from "../assets/ogp.png"
+import ogpThumbnail from "../assets/ogp2.png"
 
 const SEO = ({ description, lang, meta, title }) => {
   const { site } = useStaticQuery(
