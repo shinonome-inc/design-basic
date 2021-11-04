cca489c06d2a92b45a4099cb3717fe537b79fcd5
ayakotanaka0224 <ayakosomethingxxx@gmail.com>
2021-11-05 02:19:49 +0900

    flow-chart

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,228 +4,228 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import topImg from "../assets/top-img.svg"
-import Brush from "../assets/particle/brush.svg"
-import ColorLens from "../assets/particle/color_lens.svg"
-import Colorize from "../assets/particle/colorize.svg"
-import Create from "../assets/particle/create.svg"
-import FormatColorFill from "../assets/particle/format_color_fill.svg"
-import FormatColorText from "../assets/particle/format_color_text.svg"
-import FormatPaint from "../assets/particle/format_paint.svg"
-import FormatShapes from "../assets/particle/format_shapes.svg"
-import FormatSize from "../assets/particle/format_size.svg"
-import NearMe from "../assets/particle/near_me.svg"
+// import Brush from "../assets/particle/brush.svg"
+// import ColorLens from "../assets/particle/color_lens.svg"
+// import Colorize from "../assets/particle/colorize.svg"
+// import Create from "../assets/particle/create.svg"
+// import FormatColorFill from "../assets/particle/format_color_fill.svg"
+// import FormatColorText from "../assets/particle/format_color_text.svg"
+// import FormatPaint from "../assets/particle/format_paint.svg"
+// import FormatShapes from "../assets/particle/format_shapes.svg"
+// import FormatSize from "../assets/particle/format_size.svg"
+// import NearMe from "../assets/particle/near_me.svg"
 
-import { tsParticles } from "tsparticles"
+// import { tsParticles } from "tsparticles"
 
 const BlogIndex = ({ data, location }) => {
-  tsParticles.load("tsparticles", {
-    detectRetina: true,
-    fpsLimit: 20,
-    interactivity: {
-      detectsOn: "canvas",
-      events: {
-        onClick: {
-          enable: true,
-          mode: "push",
-        },
-        onDiv: {
-          elementId: "repulse-div",
-          enable: false,
-          mode: "repulse",
-        },
-        onHover: {
-          enable: false,
-          mode: "bubble",
-          parallax: {
-            enable: false,
-            force: 60,
-            smooth: 10,
-          },
-        },
-        resize: true,
-      },
-      modes: {
-        bubble: {
-          distance: 400,
-          duration: 2,
-          opacity: 1,
-          size: 40,
-          speed: 3,
-        },
-        connect: {
-          distance: 80,
-          lineLinked: {
-            opacity: 0.5,
-          },
-          radius: 60,
-        },
-        grab: {
-          distance: 400,
-          lineLinked: {
-            opacity: 1,
-          },
-        },
-        push: {
-          quantity: 4,
-        },
-        remove: {
-          quantity: 2,
-        },
-        repulse: {
-          distance: 200,
-          duration: 0.4,
-        },
-      },
-    },
-    particles: {
-      color: {
-        value: "#ffffff",
-      },
-      lineLinked: {
-        blink: false,
-        color: "#000",
-        consent: false,
-        distance: 150,
-        enable: false,
-        opacity: 0,
-        width: 0,
-      },
-      rotate: {
-        value: 0,
-        random: true,
-        direction: "clockwise",
-        animation: {
-          enable: true,
-          speed: 3,
-          sync: false,
-        },
-      },
-      move: {
-        attract: {
-          enable: false,
-          rotateX: 600,
-          rotateY: 1200,
-        },
-        bounce: false,
-        direction: "none",
-        enable: true,
-        outMode: "bounce",
-        random: false,
-        speed: 2,
-        straight: false,
-      },
-      number: {
-        density: {
-          enable: true,
-          area: 800,
-        },
-        limit: 0,
-        value: 20,
-      },
-      opacity: {
-        animation: {
-          enable: false,
-          minimumValue: 0.1,
-          speed: 1,
-          sync: false,
-        },
-        random: false,
-        value: 0.8,
-      },
-      shape: {
-        character: {
-          fill: false,
-          font: "Verdana",
-          style: "",
-          value: "*",
-          weight: "400",
-        },
-        image: [
-          {
-            src: Brush,
-            width: 32,
-            height: 32,
-          },
-          {
-            src: ColorLens,
-            width: 32,
-            height: 32,
-          },
-          {
-            src: Colorize,
-            width: 32,
-            height: 32,
-          },
-          {
-            src: Create,
-            width: 32,
-            height: 32,
-          },
-          {
-            src: FormatColorFill,
-            width: 32,
-            height: 32,
-          },
-          {
-            src: FormatColorText,
-            width: 32,
-            height: 32,
-          },
-          {
-            src: FormatPaint,
-            width: 32,
-            height: 32,
-          },
-          {
-            src: FormatShapes,
-            width: 32,
-            height: 32,
-          },
-          {
-            src: FormatSize,
-            width: 32,
-            height: 32,
-          },
-          {
-            src: NearMe,
-            width: 32,
-            height: 32,
-          },
-        ],
-        polygon: {
-          nb_sides: 5,
-        },
-        stroke: {
-          color: "#000000",
-          width: 0,
-        },
-        type: "image",
-      },
-      size: {
-        animation: {
-          enable: false,
-          minimumValue: 0.1,
-          speed: 10,
-          sync: false,
-        },
-        random: false,
-        value: 16,
-      },
-    },
-    polygon: {
-      draw: {
-        enable: false,
-        lineColor: "#ffffff",
-        lineWidth: 0.5,
-      },
-      move: {
-        radius: 10,
-      },
-      scale: 1,
-      type: "none",
-      url: "",
-    },
-  })
+  // tsParticles.load("tsparticles", {
+  //   detectRetina: true,
+  //   fpsLimit: 20,
+  //   interactivity: {
+  //     detectsOn: "canvas",
+  //     events: {
+  //       onClick: {
+  //         enable: true,
+  //         mode: "push",
+  //       },
+  //       onDiv: {
+  //         elementId: "repulse-div",
+  //         enable: false,
+  //         mode: "repulse",
+  //       },
+  //       onHover: {
+  //         enable: false,
+  //         mode: "bubble",
+  //         parallax: {
+  //           enable: false,
+  //           force: 60,
+  //           smooth: 10,
+  //         },
+  //       },
+  //       resize: true,
+  //     },
+  //     modes: {
+  //       bubble: {
+  //         distance: 400,
+  //         duration: 2,
+  //         opacity: 1,
+  //         size: 40,
+  //         speed: 3,
+  //       },
+  //       connect: {
+  //         distance: 80,
+  //         lineLinked: {
+  //           opacity: 0.5,
+  //         },
+  //         radius: 60,
+  //       },
+  //       grab: {
+  //         distance: 400,
+  //         lineLinked: {
+  //           opacity: 1,
+  //         },
+  //       },
+  //       push: {
+  //         quantity: 4,
+  //       },
+  //       remove: {
+  //         quantity: 2,
+  //       },
+  //       repulse: {
+  //         distance: 200,
+  //         duration: 0.4,
+  //       },
+  //     },
+  //   },
+  //   particles: {
+  //     color: {
+  //       value: "#ffffff",
+  //     },
+  //     lineLinked: {
+  //       blink: false,
+  //       color: "#000",
+  //       consent: false,
+  //       distance: 150,
+  //       enable: false,
+  //       opacity: 0,
+  //       width: 0,
+  //     },
+  //     rotate: {
+  //       value: 0,
+  //       random: true,
+  //       direction: "clockwise",
+  //       animation: {
+  //         enable: true,
+  //         speed: 3,
+  //         sync: false,
+  //       },
+  //     },
+  //     move: {
+  //       attract: {
+  //         enable: false,
+  //         rotateX: 600,
+  //         rotateY: 1200,
+  //       },
+  //       bounce: false,
+  //       direction: "none",
+  //       enable: true,
+  //       outMode: "bounce",
+  //       random: false,
+  //       speed: 2,
+  //       straight: false,
+  //     },
+  //     number: {
+  //       density: {
+  //         enable: true,
+  //         area: 800,
+  //       },
+  //       limit: 0,
+  //       value: 20,
+  //     },
+  //     opacity: {
+  //       animation: {
+  //         enable: false,
+  //         minimumValue: 0.1,
+  //         speed: 1,
+  //         sync: false,
+  //       },
+  //       random: false,
+  //       value: 0.8,
+  //     },
+  //     shape: {
+  //       character: {
+  //         fill: false,
+  //         font: "Verdana",
+  //         style: "",
+  //         value: "*",
+  //         weight: "400",
+  //       },
+  //       image: [
+  //         {
+  //           src: Brush,
+  //           width: 32,
+  //           height: 32,
+  //         },
+  //         {
+  //           src: ColorLens,
+  //           width: 32,
+  //           height: 32,
+  //         },
+  //         {
+  //           src: Colorize,
+  //           width: 32,
+  //           height: 32,
+  //         },
+  //         {
+  //           src: Create,
+  //           width: 32,
+  //           height: 32,
+  //         },
+  //         {
+  //           src: FormatColorFill,
+  //           width: 32,
+  //           height: 32,
+  //         },
+  //         {
+  //           src: FormatColorText,
+  //           width: 32,
+  //           height: 32,
+  //         },
+  //         {
+  //           src: FormatPaint,
+  //           width: 32,
+  //           height: 32,
+  //         },
+  //         {
+  //           src: FormatShapes,
+  //           width: 32,
+  //           height: 32,
+  //         },
+  //         {
+  //           src: FormatSize,
+  //           width: 32,
+  //           height: 32,
+  //         },
+  //         {
+  //           src: NearMe,
+  //           width: 32,
+  //           height: 32,
+  //         },
+  //       ],
+  //       polygon: {
+  //         nb_sides: 5,
+  //       },
+  //       stroke: {
+  //         color: "#000000",
+  //         width: 0,
+  //       },
+  //       type: "image",
+  //     },
+  //     size: {
+  //       animation: {
+  //         enable: false,
+  //         minimumValue: 0.1,
+  //         speed: 10,
+  //         sync: false,
+  //       },
+  //       random: false,
+  //       value: 16,
+  //     },
+  //   },
+  //   polygon: {
+  //     draw: {
+  //       enable: false,
+  //       lineColor: "#ffffff",
+  //       lineWidth: 0.5,
+  //     },
+  //     move: {
+  //       radius: 10,
+  //     },
+  //     scale: 1,
+  //     type: "none",
+  //     url: "",
+  //   },
+  // })
 
   const top = (
     <>
